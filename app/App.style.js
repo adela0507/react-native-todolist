c6de26a7d875c122d86a6eb69d845fed4de214cb
adela miclea <adelamiclea709@yahoo.com>
2024-12-12 21:59:39 +0200

    .

---
+++ app/App.style.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export const s = StyleSheet.create({
+  app: {
+    flex: 1,
+    backgroundColor: "#F9F9F9",
+  },
+  header: { flex: 1 },
+  body: { flex: 5, backgroundColor:"red", },
+  footer: { height: 70, backgroundColor:"blue", },
+});
---
+++ app/index.tsx
@@ -0,0 +1,22 @@
+import { Text, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { s } from "./App.style";
+export default function App() {
+  return (
+    <>
+      <SafeAreaProvider>
+        <SafeAreaView style={s.app}>
+          <View style={s.header}>
+            <Text>Header</Text>
+          </View>
+          <View style={s.body}>
+            <Text>Body</Text>
+          </View>
+        </SafeAreaView>
+      </SafeAreaProvider>
+      <View style={s.footer}>
+        <Text>Footer</Text>
+      </View>
+    </>
+  );
+}
