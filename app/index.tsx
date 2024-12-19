9d3d582ad6138d5563016d2720840d0bed191962
adela miclea <adelamiclea709@yahoo.com>
2024-12-19 11:08:21 +0200

    Add Todo

--- app/index.tsx
+++ app/index.tsx
@@ -2,26 +2,20 @@ import { Alert, ScrollView, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { s } from "./App.style";
 import { Header } from "../components/Header/Header";
-import {CardToDo} from "../components/CardToDo/CardToDo";
-import {TopBottomMenu} from "../components/TopBottomMenu/TopBottomMenu";
-import { useState } from "react";
+import { CardToDo } from "../components/CardToDo/CardToDo";
+import { useEffect, useState } from "react";
+import { TopBottomMenu } from "../components/TopBottomMenu/TopBottomMenu";
+import { ButtonAdd } from "../components/ButtonAdd/ButtonAdd";
+import Dialog from "react-native-dialog";
+import uuid from "react-native-uuid";
 
 export default function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [selectedTabName, setSelectedTabName] = useState("all");
+  const [isAddDialogDisplayed, setIsAddDialogDisplayed] = useState(false);
+  const [inputValue, setInputValue] = useState("");
 
-  const[todoList, setToDoList]=useState([
-    { id: 1, title: "Walk the dog", isCompleted: true },
-    { id: 2, title: "Go to the dentist", isCompleted: false },
-    { id: 3, title: "Learn React Native", isCompleted: false },
-    { id: 5, title: "Go to the dentist", isCompleted: false },
-    { id: 6, title: "Learn React Native", isCompleted: false },
-    { id: 7, title: "Walk the dog", isCompleted: true },
-    { id: 8, title: "Go to the dentist", isCompleted: false },
-    { id: 9, title: "Learn React Native", isCompleted: false },
-  ]);
-
-const [selectedTabName, setSelectedTabName]=useState("all");
-
-function getFilteredList() {
+  function getFilteredList() {
     switch (selectedTabName) {
       case "all":
         return todoList;
@@ -32,54 +26,98 @@ function getFilteredList() {
     }
   }
 
-function deleteTodo(todoToDelete){
-Alert.alert("Delete todo", "Are you sure you want to delete this todo?",
-  [
-    {text:"Delete", style:"destructive", onPress:()=>{
-      setToDoList(todoList.filter(t =>t.id !== todoToDelete.id));
-    }},
-    {text:"Cancel", style:"cancel", },
-  ]
-);
-}
+  function deleteTodo(todoToDelete) {
+    Alert.alert("Delete todo", "Are you sure you want to delete this todo ?", [
+      {
+        text: "Delete",
+        style: "destructive",
+        onPress: () => {
+          setTodoList(todoList.filter((t) => t.id !== todoToDelete.id));
+        },
+      },
+      { text: "Cancel", style: "cancel" },
+    ]);
+  }
 
-function renderTodoList(){
-  return getFilteredList().map((todo)=>
-  <View key={todo.id} style={s.cardItem}>
-    <CardToDo onLongPress={deleteTodo} onPress={updateTodo} todo={todo}/>
-  </View> )
-}
+  function renderTodoList() {
+    return getFilteredList().map((todo) => (
+      <View key={todo.id} style={s.cardItem}>
+        <CardToDo onLongPress={deleteTodo} onPress={updateTodo} todo={todo} />
+      </View>
+    ));
+  }
 
-function updateTodo(todo){
-  const updatedTodo={
-    ...todo, isCompleted: !todo.isCompleted
-  };
-  const updatedTodoList=[...todoList]
-  const indexUpdatedTodo=updatedTodoList.findIndex(
-    t=>t.id==updatedTodo.id);
-    updatedTodoList[indexUpdatedTodo]=updatedTodo;
-    setToDoList(updatedTodoList);
-}
+  function updateTodo(todo) {
+    const updatedTodo = {
+      ...todo,
+      isCompleted: !todo.isCompleted,
+    };
+    const updatedTodoList = [...todoList];
+    const indexToUpdate = updatedTodoList.findIndex(
+      (t) => t.id === updatedTodo.id
+    );
+    updatedTodoList[indexToUpdate] = updatedTodo;
+    setTodoList(updatedTodoList);
+  }
+
+  function addTodo() {
+    const newTodo = {
+      id: uuid.v4(),
+      title: inputValue,
+      isCompleted: false,
+    };
+    setTodoList([...todoList, newTodo]);
+    setIsAddDialogDisplayed(false);
+    setInputValue("");
+  }
+
+  function renderAddDialog() {
+    return (
+      <Dialog.Container
+        visible={isAddDialogDisplayed}
+        onBackdropPress={() => setIsAddDialogDisplayed(false)}
+      >
+        <Dialog.Title>Add todo</Dialog.Title>
+        <Dialog.Description>Choose a name for your todo</Dialog.Description>
+        <Dialog.Input
+          onChangeText={setInputValue}
+          placeholder="Ex : Go to the dentis"
+        />
+        <Dialog.Button
+          label="Cancel"
+          color="grey"
+          onPress={() => setIsAddDialogDisplayed(false)}
+        />
+        <Dialog.Button
+          disabled={inputValue.length === 0}
+          label="Save"
+          onPress={addTodo}
+        />
+      </Dialog.Container>
+    );
+  }
 
   return (
     <>
       <SafeAreaProvider>
         <SafeAreaView style={s.app}>
           <View style={s.header}>
-            <Header/>
+            <Header />
           </View>
           <View style={s.body}>
-            <ScrollView>
-            {renderTodoList()}
-            </ScrollView>
+            <ScrollView>{renderTodoList()}</ScrollView>
           </View>
+          <ButtonAdd onPress={() => setIsAddDialogDisplayed(true)} />
         </SafeAreaView>
       </SafeAreaProvider>
       <View style={s.footer}>
-        <TopBottomMenu 
-        todoList={todoList}
-        onPress={setSelectedTabName} selectedTabName={selectedTabName} />
+        <TopBottomMenu
+          todoList={todoList}
+          onPress={setSelectedTabName}
+          selectedTabName={selectedTabName}
+        />
       </View>
+      {renderAddDialog()}
     </>
   );
 }
---
+++ components/ButtonAdd/ButtonAdd.style.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+export const s=StyleSheet.create({
+    btn:{
+        position:"absolute",
+        bottom:60,
+        right:30,
+        backgroundColor:"#c2daff",
+        paddingHorizontal:30,
+        paddingVertical:15,
+        borderRadius:7,
+        },
+    txt:{
+        color:"#2f76e5",
+        fontWeight:"bold",
+        fontSize:18,
+    },
+
+});
